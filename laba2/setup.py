9af2b601c651aa36b40428e0e905cc9ad6fd6d38
Artem <lebedev20211@gmail.com>
2022-05-07 13:42:06 +0300

    fixed mistake2

--- laba2/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-      name='Json, yaml and toml serializer',
-      version='0.9',
-      author='Lebedev Artem',
-      packages=['serializers'],
-     )
